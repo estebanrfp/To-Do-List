0a9e577da1a7a344bd679ceea01f84bd1160c89a
estebanrfp <estebanrfp@gmail.com>
2025-07-30 21:09:03 +0200

    update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { GDB } from "https://cdn.jsdelivr.net/npm/gdb-p2p/+esm";
+import { GDB } from "https://cdn.jsdelivr.net/npm/genosdb/+esm";
 
 document.addEventListener('DOMContentLoaded', async () => {
   const taskInput = document.getElementById('task-input');
